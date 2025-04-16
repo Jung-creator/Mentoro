7fac5d895f85f3052ecd26a53b807a73d7eb1a27
Jung <junchoia25@pos.idserve.net>
2025-04-16 23:38:35 +0900

    Initial Commit

---
+++ Mentoro/MentoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentoroApp.swift
+//  Mentoro
+//
+//  Created by MacBookAir on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MentoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MentoroTests/MentoroTests.swift
@@ -0,0 +1,16 @@
+//
+//  MentoroTests.swift
+//  MentoroTests
+//
+//  Created by MacBookAir on 4/16/25.
+//
+
+import Testing
+
+struct MentoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

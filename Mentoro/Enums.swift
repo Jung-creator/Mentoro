f0cec552060470b3c4b1a2f06076f735a044901d
Jung <junchoia25@pos.idserve.net>
2025-04-17 00:13:43 +0900

    Add enums

---
+++ Mentoro/Enums.swift
@@ -0,0 +1,44 @@
+//
+//  MockData.swift
+//  Mentoro
+//
+//  Created by MacBookAir on 4/16/25.
+//
+
+enum Name: String, CaseIterable {
+    case Dora
+    case Gommin
+    case Daisy
+    case Nathan
+    case Lumi
+    case Leeo
+    case Lingo
+    case Saya
+    case Sup
+    case Isaac
+    case MK
+    case Jason
+    case Judy
+    case Jiku
+    case Friday
+    case Howard
+}
+
+enum Part: String, CaseIterable {
+    case development
+    case design
+    case business
+}
+
+
+enum Category: String, CaseIterable {
+    case development
+    case design
+    case business
+    case softSkills
+    case games
+    case sports
+    case food
+    case music
+    case contents
+}

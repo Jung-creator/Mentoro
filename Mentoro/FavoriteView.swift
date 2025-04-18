9ad77098ca044409c8655d5400268123c9032cf1
Jung <junchoia25@pos.idserve.net>
2025-04-18 17:20:29 +0900

    feat(ui): structure favorite card item view

--- Mentoro/FavoriteView.swift
+++ Mentoro/FavoriteView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct FavoriteView: View {
     var body: some View {
-        let columns = [GridItem(.adaptive(minimum: 50), spacing: 4)]
+//        let columns = [GridItem(.adaptive(minimum: 50), spacing: 4)]
         
         let items = ["100", "2000", "30000", "4000", "50", "600", "7000", "8000"]
         
@@ -18,17 +18,23 @@ struct FavoriteView: View {
             
             
             VStack(alignment: .leading) {
+                Spacer()
+                    .frame(height: 16)
                 Text("Title")
                     .font(.title.bold())
                 ZStack(alignment: .leading) {
+                    Color.gray
+                        .opacity(0.2)
+                        .frame(height: width * 0.25)
+                        .cornerRadius(16)
                     HStack(alignment: .top) {
                         Color.gray
                             .opacity(0.2)
-                            .frame(width: width * 0.25 - 16,height: width * 0.25 - 16)
+                            .frame(width: width * 0.25 - 16, height: width * 0.25 - 16)
                             .cornerRadius(12)
                         VStack(alignment: .leading) {
                             Spacer()
-                                .frame(height: 4)
+                                .frame(height:4)
                             HStack {
                                 Text("Name")
                                     .font(.body.bold())
@@ -37,22 +43,18 @@ struct FavoriteView: View {
                                     .font(.caption.bold())
                                     .foregroundStyle(Color.gray)
                             }.padding(.trailing)
-                            
-                            HStack {
-                                Text(items.first!)
-                            }
-                            
+                            Spacer()
+                            Text("text")
+                                .padding(.bottom)
                             
                         }
                         
-                    }.padding(.horizontal,8)
-                    
-                    
-                    Color.gray
-                        .opacity(0.2)
-                        .frame(height: width * 0.25)
-                        .cornerRadius(16)
+                    }
+                    .padding(.horizontal,8)
+                    .padding(.top,8)
                 }
+                // ZStack의 크기를 정해주는 것이 중요
+                .frame(height: width * 0.25)
 
             }
             .padding()

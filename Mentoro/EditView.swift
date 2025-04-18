a141484f8ab669be7c4d984009a41041f856aaac
Jung <junchoia25@pos.idserve.net>
2025-04-18 23:23:44 +0900

    feat(ui): structure layout for Edit view

--- Mentoro/EditView.swift
+++ Mentoro/EditView.swift
@@ -8,10 +8,119 @@
 import SwiftUI
 
 struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader {
+            geometry in
+            let width = geometry.size.width
+            ScrollView {
+                VStack {
+                    Spacer()
+                    .frame(height: 32)
+                    HStack {
+                        Spacer()
+                            .frame(width:24)
+                        Text("Name")
+                            .font(.headline)
+                        Button(action: {
+                            print("Choose name")
+                        }) {
+                            Image(systemName: "chevron.up.chevron.down")
+                            
+                        }
+                    }
+                    Circle()
+                        .fill(Color.gray)
+                        .opacity(0.2)
+                        .frame(width: 144)
+                        .padding(.bottom)
+                    
+                    Group {
+                        Color.gray
+                            .frame(height:1)
+                            .padding(.horizontal, width * 0.1)
+                            .opacity(0.2)
+                        
+                        HStack {
+                            ImageAndSelectView()
+                            
+                            Rectangle()
+                                .fill(Color.gray)
+                                .opacity(0.2)
+                                .frame(width: 1, height: 32)
+                            ImageAndSelectView()
+                            Rectangle()
+                                .fill(Color.gray)
+                                .opacity(0.2)
+                                .frame(width: 1, height: 32)
+                            ImageAndSelectView()
+                        }
+                        Color.gray
+                            .frame(height:1)
+                            .opacity(0.2)
+                            .padding(.horizontal, width * 0.1)
+                            .padding(.top,4)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        print("Save tapped")
+                        dismiss()
+                    }
+                }
+                
+            }
+        }
+        
+        
     }
 }
+
+struct ImageAndSelectView: View {
+    @State private var showPicker = false
+    @State private var selectedItem = "10"
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .opacity(0)
+            VStack {
+                Image(systemName: "star.fill")
+                    .font(.title)
+                    .foregroundColor(.gray)
+                    .opacity(0.3)
+                    .padding(.top,8)
+                
+                Button(action: {
+                    showPicker = true
+                    print("Button Tapped, showPicker: \(showPicker)")
+                }) {
+                    Picker("Select Choice", selection: $selectedItem) {
+                        ForEach(["10", "2000", "3000", "4000"], id: \.self) { item in
+                            Text(item)
+                                .font(.caption)
+                            
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal,8)
+        }
+        .frame(width: 80)
+    }
+}
+
+//var options = ["옵션1", "옵션2", "옵션3", "옵션4"]
+//@State private var selectionOption = 0
+//
+//Picker("Select Choice", selection: $selectionOption) {
+//  ForEach(0 ..< options.count) {
+//    Text(options[$0])
+//  }
+//}
 
 #Preview {
     EditView()
--- Mentoro/MentorDetailView.swift
+++ Mentoro/MentorDetailView.swift
@@ -9,79 +9,72 @@ import SwiftUI
 
 struct MentorDetailView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                Spacer()
-                    .frame(height: 24)
-                Text("Name")
-                    .font(.title2.bold())
-                Circle()
-                    .fill(Color.gray)
-                    .opacity(0.2)
-                    .frame(width: 144)
-                    .padding(.bottom)
-
-                Group {
-                    Color.gray
-                        .frame(height:1)
+        GeometryReader {
+            geometry in
+            let width = geometry.size.width
+            
+            ScrollView {
+                VStack {
+                    Spacer()
+                        .frame(height: 24)
+                    Text("Name")
+                        .font(.title2.bold())
+                    Circle()
+                        .fill(Color.gray)
                         .opacity(0.2)
-                                    
-                    HStack {
-                        VStack {
-                            Image(systemName: "star.fill")
-                                .font(.title)
-                                .foregroundColor(.gray)
-                                .opacity(0.3)
-                                .padding(.all,8)
-                            Text("title")
-                                .font(.caption)
-                                .foregroundStyle(Color.gray)
-                                .opacity(0.5)
-                        }.padding(.horizontal)
-                        Rectangle()
-                            .fill(Color.gray)
+                        .frame(width: 144)
+                        .padding(.bottom)
+                    
+                    Group {
+                        Color.gray
+                            .frame(height:1)
                             .opacity(0.2)
-                            .frame(width: 1, height: 32)
-                        VStack {
-                            Image(systemName: "star.fill")
-                                .font(.title)
-                                .foregroundColor(.gray)
-                                .opacity(0.3)
-                                .padding(.all,8)
-                            Text("title")
-                                .font(.caption)
-                                .foregroundStyle(Color.gray)
-                                .opacity(0.5)
-                        }.padding(.horizontal)
-                        Rectangle()
-                            .fill(Color.gray)
+                            .padding(.horizontal, width * 0.1)
+                        
+                        HStack {
+                            ImageAndTextView()
+                            
+                            Rectangle()
+                                .fill(Color.gray)
+                                .opacity(0.2)
+                                .frame(width: 1, height: 32)
+                            ImageAndTextView()
+                            Rectangle()
+                                .fill(Color.gray)
+                                .opacity(0.2)
+                                .frame(width: 1, height: 32)
+                            ImageAndTextView()
+                        }
+                        Color.gray
+                            .frame(height:1)
                             .opacity(0.2)
-                            .frame(width: 1, height: 32)
-                        VStack {
-                            Image(systemName: "star.fill")
-                                .font(.title)
-                                .foregroundColor(.gray)
-                                .opacity(0.3)
-                                .padding(.all,8)
-                            Text("title")
-                                .font(.caption)
-                                .foregroundStyle(Color.gray)
-                                .opacity(0.5)
-                        }.padding(.horizontal)
+                            .padding(.top,4)
+                            .padding(.horizontal, width * 0.1)
                     }
-                    Color.gray
-                        .frame(height:1)
-                        .opacity(0.2)
-                        .padding(.top,4)
-                }.padding(.horizontal,32)
-            }.toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Edit") {
-                        print("Edit tapped")
+                }.toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        
+                        NavigationLink("Edit", destination: EditView())
                     }
                 }
             }
         }
+    }
+}
+
+struct ImageAndTextView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "star.fill")
+                .font(.title)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+                .padding(.all,8)
+            Text("title")
+                .font(.caption)
+                .foregroundStyle(Color.gray)
+                .opacity(0.5)
+        }.padding(.horizontal)
     }
 }
 

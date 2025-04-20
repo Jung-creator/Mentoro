18911e6ce971e4292f6b350b4106dec34c2efced
Jung <junchoia25@pos.idserve.net>
2025-04-20 16:25:06 +0900

    feat(ui): build top section of MainView

--- Mentoro/MainView.swift
+++ Mentoro/MainView.swift
@@ -22,33 +22,55 @@ struct MainView: View {
             ScrollView {
                 VStack(alignment: .leading) {
                     Spacer()
-                        .frame(height:32)
+                        .frame(height: 16)
                     
                     // MARK: First View
-                    Text("Title")
+                    HStack {
+                        Spacer()
+                            .frame(width: 8)
+                        Text("오늘의 멘토")
+                    }
+                    
                         .font(.title.bold())
                     ZStack(alignment: .bottomLeading) {
-                        Color.gray.opacity(0.2)
-                            .frame(height: width * 1.2)
+                        Image("lingo")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: width - 32,height: width * 1.2)
+                            .clipped() // 이미지가 넘치는 부분 자르기
                             .cornerRadius(16)
+                        LinearGradient(
+                            colors: [Color.blue, Color.blue.opacity(0.3),Color.blue.opacity(0)],
+                            startPoint: .bottom,
+                            endPoint: .center
+                        ).clipShape(RoundedCorner(radius: 16, corners: [.bottomLeft, .bottomRight]))
+                        LinearGradient(
+                            colors: [Color.blue, Color.blue.opacity(0),.blue.opacity(0)],
+                            startPoint: .bottom,
+                            endPoint: .center
+                        ).clipShape(RoundedCorner(radius: 16, corners: [.bottomLeft, .bottomRight]))
                         
                         HStack(alignment: .bottom) {
                             VStack(alignment: .leading) {
-                                Text("Inner Text")
+                                Text("어느 날 갑자기")
                                     .font(.body.bold())
-                                    .foregroundStyle(Color.gray)
-                                Text("Inner Text")
-                                    .font(.title3.bold())
+                                    .foregroundColor(.teal)
+                                    .saturation(3)
+                                Text("피아노가 생긴 Lingo")
+                                    .font(.title2.bold())
+                                    .foregroundColor(Color.white)
                                 
                             }
                             Spacer()
                             Button(action: {
                                 print("Button pressed")
                             }) {
-                                Text("See more")
-                                
+                                Text("더보기").bold()
+
                             }
-                            .buttonStyle(.bordered)
+                            .foregroundStyle(Color.accentColor)
+                            .buttonStyle(.borderedProminent)
+                            .tint(.white)
                             .cornerRadius(20)
                         }
                         .padding()
@@ -149,6 +171,22 @@ struct MainView: View {
             }
         }
     }
+
+
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = 16.0
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
 
 #Preview {
     MainView()
